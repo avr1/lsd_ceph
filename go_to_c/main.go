54296ec43883317e2cc49a99b9789681cf036a98
Arjun Ramachandrula <avrdev77@gmail.com>
2021-10-21 23:16:44 -0400

    Started writing ceph shell script

--- go_to_c/main.go
+++ go_to_c/main.go
@@ -1,14 +1,14 @@
 package main
 
-// #cgo CFLAGS: -g -Wall 
+// #cgo CFLAGS: -g -Wall
 // #include <stdlib.h>
 // #include "f_to_c.h"
 import "C"
 
 import "fmt"
 
-func main(){
-    fmt.Print("32 degrees Fahrenheit is:")
-    c := C.f_to_c(322)
-    fmt.Println(c)
+func main() {
+	fmt.Print("32 degrees Fahrenheit is:")
+	c := C.f_to_c(32)
+	fmt.Println(c)
 }
---
+++ script/setup-ceph.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+sudo apt-get update && sudo apt-get install git
+
+git clone https://github.com/ceph/ceph
+
+
