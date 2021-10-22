a6f4d0ee573dafb971b77a28e718d39567205e7f
Arjun Ramachandrula <avrdev77@gmail.com>
2021-10-21 23:30:15 -0400

    First version of full-on vstart, needs VM testing

--- script/setup-ceph.sh
+++ script/setup-ceph.sh
@@ -1,7 +1,21 @@
 #! /bin/sh
 
-sudo apt-get update && sudo apt-get install git
+sudo apt-get update && sudo apt-get install git cmake
 
-git clone https://github.com/ceph/ceph
+git clone https://github.com/ceph/ceph --depth 1
+
+cd ceph
+
+./run-make-check.sh
+
+cmake --build build
+
+cd build
+
+env MON=1 MDS=1 ../src/vstart.sh -d -n -x
+
+
+
+
 
 
